69cc21c206334d27dc76c9fe37732e8bafd4902b
PremPrakashHessa <premprakash8721@gmail.com>
2023-11-14 22:35:55 +0530

    first commit

---
+++ src/main/java/com/example/ostocklicensingservice/OStockLicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ostocklicensingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OStockLicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OStockLicensingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ostocklicensingservice/OStockLicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ostocklicensingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OStockLicensingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
